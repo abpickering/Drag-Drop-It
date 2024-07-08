e41c58dae1a5feae4d5ccf3a61c03ecc26b49ee1
arichards5dm <arichards5@dmacc.edu>
2024-07-08 15:18:39 -0500

    Add files via upload

---
+++ drag and drop it like its hot/script.js
@@ -0,0 +1,40 @@
+function dragElement(terrariumElement) {
+	//set 4 positions for positioning on the screen
+	let pos1 = 0,
+		pos2 = 0,
+		pos3 = 0,
+		pos4 = 0;
+	terrariumElement.onpointerdown = pointerDrag;
+
+
+function pointerDrag(e) {
+	e.preventDefault();
+	console.log(e);
+	pos3 = e.clientX;
+	pos4 = e.clientY;
+    document.onpointermove = elementDrag;
+    document.onpointerup = stopElementDrag;
+}
+
+function elementDrag(e) {
+	pos1 = pos3 - e.clientX;
+	pos2 = pos4 - e.clientY;
+	pos3 = e.clientX;
+	pos4 = e.clientY;
+	console.log(pos1, pos2, pos3, pos4);
+	terrariumElement.style.top = terrariumElement.offsetTop - pos2 + 'px';
+	terrariumElement.style.left = terrariumElement.offsetLeft - pos1 + 'px';
+}
+
+function stopElementDrag() {
+	document.onpointerup = null;
+	document.onpointermove = null;
+}
+}
+
+dragElement(document.getElementById('cherry'));
+dragElement(document.getElementById('chocolate-syrup'));
+dragElement(document.getElementById('caramel-syrup'));
+dragElement(document.getElementById('sprinkles'));
+dragElement(document.getElementById('peanuts'));
+dragElement(document.getElementById('whip-cream'));
